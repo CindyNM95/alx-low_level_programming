87268f8e584c7dbf190c4cddf13c97de754ae1d9
CindyNM95 <mabhenanc@gmail.com>
2023-04-21 18:39:06 -0700

    C programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
-include<stdio.h>
+#include <stdio.h>
 /**
-*main - A program that prints a line with puts function
+* main - A program that prints a line with puts function
 *
-*Return Always 0 (Success)
+* Return Always 0 (Success)
 */
 int main(void)
 {
